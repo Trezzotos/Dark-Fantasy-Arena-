9e6e22243ce4377db7b71b3714684736a0b847c1
Trezzotos <ivandimaria04@gmail.com>
2025-10-07 10:58:54 +0200

    Revert "Refactoring Main Menu"
    This reverts commit aa827f15e381fc76f17370d6457e0bbfff3b9dac.

--- Assets/Scripts/Main Menu Script/AudioManager.cs
+++ Assets/Scripts/Main Menu Script/AudioManager.cs
@@ -2,22 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
+public class AudioManager : MonoBehaviour
+{
     public static AudioManager Instance { get; private set; }
-    [SerializeField] AudioSource musicSource;
-    void Awake() {
-        if(Instance != null) { Destroy(gameObject); return; }
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("Clips Registrati")]
+    [SerializeField] private List<AudioClip> musicClips;
+    [SerializeField] private List<AudioClip> sfxClips;
+
+    private Dictionary<string, AudioClip> musicDict;
+    private Dictionary<string, AudioClip> sfxDict;
+
+    // Singleton design pattern
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);  // evita duplicati
+            return;
+        }
+
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        InitializeDictionaries();
     }
 
-    void OnEnable() => EventBus.Subscribe(OnEvent);
-    void OnDisable() => EventBus.Unsubscribe(OnEvent);
+    private void InitializeDictionaries()
+    {
+        musicDict = new Dictionary<string, AudioClip>();
+        sfxDict = new Dictionary<string, AudioClip>();
 
-    void OnEvent(string topic, object payload) {
-        if(topic == "Menu/StartGame") musicSource.Stop();
-        if(topic == "UI/ShowPanel" && (string)payload == "Main") PlayMenuMusic();
+        foreach (var clip in musicClips)
+            musicDict[clip.name] = clip;
+
+        foreach (var clip in sfxClips)
+            sfxDict[clip.name] = clip;
     }
 
-    void PlayMenuMusic() => musicSource.Play();
+    // Riproduzione musica
+    public void PlayMusic(string clipName, bool loop = true)
+    {
+        if (musicDict.TryGetValue(clipName, out var clip))
+        {
+            musicSource.clip = clip;
+            musicSource.loop = loop;
+            musicSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"Clip musicale '{clipName}' non trovata!");
+        }
+    }
+
+    // Effetti sonori
+    public void PlaySFX(string clipName)
+    {
+        if (sfxDict.TryGetValue(clipName, out var clip))
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogWarning($"SFX '{clipName}' non trovato!");
+        }
+    }
+
+    // Volume controlli
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = Mathf.Clamp01(volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = Mathf.Clamp01(volume);
+    }
 }
---
+++ Assets/Scripts/Main Menu Script/IMenuState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMenuState{
+    // Enter()
+    // Update()
+    // Exit()
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
