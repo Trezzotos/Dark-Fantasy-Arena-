dc54b5ac6b8c4d6f390a973dfb81f1d181122812
davyrap <77901709+davyrap@users.noreply.github.com>
2025-10-08 21:57:45 +0200

    Enemy spawner
    Enemy spawner script
    The spawner is an Entity itself, so it can be destroyed

---
+++ Assets/Scripts/Entity/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy[] enemyPool;
+    public int spawnCount = 0;
+
+    [Header("Zombie properties")]
+    public float minHealth = 1;
+    public float maxHealth = 10;
+    [Space]     // these values will be determined by the GameManager based on the level and the difficoulty
+    public float minDamage = 1;
+    public float maxDamage = 5;
+
+    void Start()
+    {
+        Debug.Log("Inizio");
+        StartCoroutine("Spawn");
+    }
+
+    public IEnumerator Spawn()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Instantiate(enemyPool[Random.Range(0, enemyPool.Length)], transform.position, transform.rotation);
+            Debug.Log(i);
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
