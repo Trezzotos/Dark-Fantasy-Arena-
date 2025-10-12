ee0bb66e6ebe88eee7dbc4b1c2d1265dec38c085
Trezzotos <ivandimaria04@gmail.com>
2025-10-12 14:28:41 +0200

    Change music
    to do

--- Assets/Scripts/Main Menu + Options/AudioClips.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class AudioClips 
-{
-    public const string MainMenuTrack = "Music/MainMenuTrack";
-    public const string FightingTrack = "Music/FightingTrack";
-}
