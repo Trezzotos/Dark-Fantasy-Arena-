ba53c6ff30efe70cf492b42544d20616fcdfbbc6
davyrap <77901709+davyrap@users.noreply.github.com>
2025-10-27 19:43:25 +0100

    MainMenu - Ranking

--- Assets/Scripts/Main Menu + Options/RankingMenu.cs
+++ Assets/Scripts/Main Menu + Options/RankingMenu.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RankingMenu : MonoBehaviour
 {
+    public TMP_Text[] nameLabels;
+    public TMP_Text[] scoreLabels;
+
+    void Start()
+    {
+        List<ScoreEntry> scores = ScoreboardManager.GetScoreboard();
+
+        int i = 0;
+        foreach (ScoreEntry record in scores)   // QUALCUNO HA DETTO RECORD?!?!?!?
+        {
+            nameLabels[i].text = record.name;
+            scoreLabels[i].text = "" + record.score;
+            i++;
+        }
+    }
+
     public void Back()
     {
         SceneManager.LoadScene("MainMenu");
     }
-
 }
